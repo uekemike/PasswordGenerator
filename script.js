4b2110f9a0cf786dbb3fc216b72e3179f2a91df7
Ufuoma Ekemike <uekemike@yahoo.com>
2021-07-12 23:15:45 -0400

    Changes again

--- script.js
+++ script.js
@@ -18,29 +18,49 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
 
-  var minLen = 8;
-  var maxLen = 128;
+  var pwdLength = prompt("Enter password length. Must be between 8 and 128 in length.");
+
+    if (isNaN(pwdLength)){
+    alert("Password must be a number");
+    return;
+  }
+  else if (pwdLength < 8 || pwdLength > 128){
+    alert("Password length cannot be less then 8 or greater than 128");
+    return;
+  } else{
+    var includeLowercaseLetter =   confirm("Would you like to include Lowercase letters");
+    var includeUpperCaseLetter =   confirm("Would you like to include Uppercase letters");
+    var includeSpecialChar =   confirm("Would you like to include Special characters");
+    var includeNumbers =   confirm("Would you like to include Numbers");
+
+  }
+
+
+  var lowerCaseLetter ="a b c d e f g h i j k l m n o p q r s t u v w x y z";
+  var lowerCaseArray = lowerCaseLetter.split(" ");
+  var upperCased = lowerCaseLetter.toUpperCase();
+  var upperCasedArray = upperCased.split(" ");
+  var specialChar="! @ # $ % ^ & * ( ) _ + ";
+  var specialCharArray = specialChar.split(" ");
+  var numbersToGenerate= "0 1 2 3 4 5 6 7 8 9";
+  var numbersToGenArray = numbersToGenerate.split(" ");
+
+  console.log(lowerCaseArray);
+  console.log(upperCasedArray);
+  console.log(specialCharArray);
+  console.log(numbersToGenArray);
+
   //var isNotAlphabet = true;
-  var pwdLength =prompt("Enter password lenght. Must be between 8 and 128 in lenght")
+  
 
   //console.log( pwdLength )
 
-  if (isNaN(pwdLength)){
-    alert("Password must be a number")
-  }
-  else if (pwdLength < minLen || pwdLength > maxLen){
-    alert("Password lenght cannot be less then 8 or greater than 128")
-  }
-  else (pwdLength > minLen || pwdLength < maxLen)
-  {
-    var includeLowercaseLetter =   confirm("Would you like to include Lowercase letters")
-    var includeUpperCaseLetter =   confirm("Would you like to include Uppercase letters")
-    var includeSpecialChar =   confirm("Would you like to include special characters")
-    var includeNumbers =   confirm("Would you like to include Numbers")
-  }
- if (includeUpperCaseLetter != true && includeLowercaseLetter != true && includeNumbers != true && includeSpecialChar != true){
-    alert("You must select at least one character type!")
+  
+
+  if (!includeUpperCaseLetter && !includeLowercaseLetter && !includeNumbers  && !includeSpecialChar){
+    alert("You must select at least one character type!");
+    return;
 }
-else(includeSpecialChar)
-  }
+
+}
 
