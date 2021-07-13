4e70496a7f6f1b6020ba5fc06a9bd6ea1ce183fa
Ufuoma Ekemike <uekemike@yahoo.com>
2021-07-12 22:25:03 -0400

    pushing some more changes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -6,7 +5,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  
+
 
   passwordText.value = password;
 
@@ -15,42 +14,33 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
+//generateBtn.addEventListener('click', generatePassword);
 
 function generatePassword(){
-  var includeLowercaseLetter ="abcdefghijklmnopqrstuvwxyz";
-  var includeUpperCaseLetter ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var includeSpecialChar = "!@#$%^&*()_";
-  var includeNumbers="0123456789";
-  var password =""
-  
+
   var minLen = 8;
   var maxLen = 128;
   //var isNotAlphabet = true;
-  var pwdLength =prompt("Enter password length. Must be between 8 and 128 in length")
+  var pwdLength =prompt("Enter password lenght. Must be between 8 and 128 in lenght")
 
   //console.log( pwdLength )
- 
+
   if (isNaN(pwdLength)){
     alert("Password must be a number")
-  }else if(pwdLength < minLen || pwdLength > maxLen){
-    alert("Password lenght cannot be less then 8 or greater than 128")
-  }else {
-    includeLowercaseLetter =   confirm("Would you like to include Lowercase letters")
-    includeUpperCaseLetter =   confirm("Would you like to include Uppercase letters")
-    includeSpecialChar =   confirm("Would you like to include special characters")
-    includeNumbers =   confirm("Would you like to include Numbers")
   }
-  if(includeLowercaseLetter){
-
-  }else if(includeSpecialChar){
-
-  }else if(includeNumbers){
-
-  } else(includeSpecialChar)
-
+  else if (pwdLength < minLen || pwdLength > maxLen){
+    alert("Password lenght cannot be less then 8 or greater than 128")
+  }
+  else (pwdLength > minLen || pwdLength < maxLen)
+  {
+    var includeLowercaseLetter =   confirm("Would you like to include Lowercase letters")
+    var includeUpperCaseLetter =   confirm("Would you like to include Uppercase letters")
+    var includeSpecialChar =   confirm("Would you like to include special characters")
+    var includeNumbers =   confirm("Would you like to include Numbers")
+  }
+ if (includeUpperCaseLetter != true && includeLowercaseLetter != true && includeNumbers != true && includeSpecialChar != true){
+    alert("You must select at least one character type!")
 }
+else(includeSpecialChar)
+  }
 
-
-
-//alert("Click OK button to generate password")
